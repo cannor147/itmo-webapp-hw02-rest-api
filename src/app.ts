f880341be6797cf34df0930dc7f40842937e9b5b
anonymous <anonymous@yandex.ru>
2020-03-26 15:24:03 +0500

    Создает задачу

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import db from './db';
+
+const app = express();
+
+/* [TODO] Код вашего приложения */
+
+export default app;
---
+++ src/db/index.ts
@@ -0,0 +1,6 @@
+import Sequelize from 'sequelize';
+
+/*[TODO] Код для подключения DB*/
+const placeholder = {};
+
+export default placeholder;
